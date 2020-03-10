fd96c799a4e57a268de14397dc2dd34e5c77e403
jojieun <history201@naver.com>
2020-03-10 15:08:31 +0900

    '광고주faq오류수정'

--- resources/views/admin/part_advertiserfaq.blade.php
+++ resources/views/admin/part_advertiserfaq.blade.php
@@ -1,9 +1,9 @@
-        @forelse ($exposures as $exposure)
+        @forelse ($advertiserfaqs as $advertiserfaq)
         <tr>
             <td>{{ $loop->iteration }}</td>
-            <td>{{ $exposure->name }}</td>
-            <td>{{ $exposure->price }}</td>
-            <td>{{ $exposure->limit }}</td>
+            <td>{{ $advertiserfaq->aFAQcategory->name }}</td>
+            <td>{{ $advertiserfaq->question }}</td>
+            <td>{{ $advertiserfaq->updated_at }}</td>
             <td>
                 <button class="edit" value="{{ $advertiserfaq->id }}">수정</button>
                 <button class="del" value="{{ $advertiserfaq->id }}">삭제</button>
@@ -11,6 +11,6 @@
         </tr>
         @empty
         <tr>
-            <td colspan=100>노출옵션이</td>
+            <td colspan=100>광고주 FAQ가 없습니다.</td>
         </tr>
         @endforelse
