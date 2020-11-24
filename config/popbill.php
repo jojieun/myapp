c1776ef86b6afad4e7363755bba58e38587a37e1
jojieun <56054760+jojieun@users.noreply.github.com>
2020-11-24 12:05:29 +0900

    Update popbill.php

--- config/popbill.php
+++ config/popbill.php
@@ -5,7 +5,7 @@ return [
     'LinkID' => 'STRONGAD',
 
     // 파트너 신청시 발급받은 비밀키
-    'SecretKey' => 'rM4PCQvezN0YqacOq6BuANeIEZqO+fjW9mLgw+MYfRQ=',
+    'SecretKey' => '',
 
     // 통신방식 기본은 CURL , PHP curl 모듈 사용에 문제가 있을 경우 STREAM 기재가능.
     // STREAM 사용시에는 php.ini의 allow_url_fopen = on 으로 설정해야함.
